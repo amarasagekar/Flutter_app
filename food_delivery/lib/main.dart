1df5bcb61f16948ec3c68e22b2c0c1b65084d5dc
Amar Asagekar <amarasagekar@gmail.com>
2023-10-08 20:23:52 +0530

    ui of spalsh screen and landing screen

---
+++ food_delivery/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/const/colors.dart';
+import 'package:food_delivery/screens/landingScreen.dart';
+import './screens/splashScreen.dart';
+import './screens/loginScreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        fontFamily: "Metropolis",
+        primarySwatch: Colors.red,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(
+              AppColor.orange,
+            ),
+            shape: MaterialStateProperty.all(
+              StadiumBorder(),
+            ),
+            elevation: MaterialStateProperty.all(0),
+          ),
+        ),
+        textTheme: TextTheme(
+          bodyText2: TextStyle(
+            color: AppColor.secondary,
+          ),
+        ),
+      ),
+      home: SplashScreen(),
+      routes: {
+        LandingScreen.routeName: (context) => LandingScreen(),
+        LoginScreen.routeName: (context) => LoginScreen(),
+      },
+    );
+  }
+}
