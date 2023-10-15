5506a2eceda9dbf2ccd088000ecf2aff5a04b623
Amar Asagekar <amarasagekar@gmail.com>
2023-10-15 07:16:46 +0530

    UI inidvidual Iteam screen

--- food_delivery/lib/screens/offerScreen.dart
+++ food_delivery/lib/screens/offerScreen.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/widgets/customNavBar.dart';
 
 class OfferScreen extends StatelessWidget {
   static const routeName = "/offerScreen";
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          SafeArea(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [],
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            child: CustomNavBar(
+              offer: true,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
