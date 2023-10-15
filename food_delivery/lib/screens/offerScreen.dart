c75d373d51a7be31f6a28911ddd76399b8fd63a6
Amar Asagekar <amarasagekar@gmail.com>
2023-10-15 20:32:39 +0530

    UI offer screen & Profile screen

--- food_delivery/lib/screens/offerScreen.dart
+++ food_delivery/lib/screens/offerScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/const/colors.dart';
+import 'package:food_delivery/utils/helper.dart';
 import 'package:food_delivery/widgets/customNavBar.dart';
 
 class OfferScreen extends StatelessWidget {
@@ -10,9 +12,82 @@ class OfferScreen extends StatelessWidget {
       body: Stack(
         children: [
           SafeArea(
-            child: SingleChildScrollView(
-              child: Column(
-                children: [],
+            child: Container(
+              height: Helper.getScreenHeight(context),
+              width: Helper.getScreenWidth(context),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Latest Offer",
+                            style: Helper.getTheme(context).headline4,
+                          ),
+                          Image.asset(
+                            Helper.getAssetName("cart.png", "virtual"),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Row(
+                        children: [
+                          Text("Find discount, offer special"),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            height: 30,
+                            width: Helper.getScreenWidth(context) * 0.4,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              child: Text("Check offers"),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    OfferCard(
+                      image: Image.asset(
+                        Helper.getAssetName("breakfast.jpg", "real"),
+                        fit: BoxFit.cover,
+                      ),
+                      name: "Cafe de Noires",
+                    ),
+                    OfferCard(
+                      image: Image.asset(
+                        Helper.getAssetName("western2.jpg", "real"),
+                        fit: BoxFit.cover,
+                      ),
+                      name: "Isso",
+                    ),
+                    OfferCard(
+                      image: Image.asset(
+                        Helper.getAssetName("coffee3.jpg", "real"),
+                        fit: BoxFit.cover,
+                      ),
+                      name: "Cafe Beans",
+                    ),
+                    SizedBox(
+                      height: 100,
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
@@ -21,6 +96,86 @@ class OfferScreen extends StatelessWidget {
             left: 0,
             child: CustomNavBar(
               offer: true,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class OfferCard extends StatelessWidget {
+  const OfferCard({
+    super.key,
+    required String name,
+    required Image image,
+  })  : _name = name,
+        _image = image;
+
+  final String _name;
+  final Image _image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      width: double.infinity,
+      child: Column(
+        children: [
+          SizedBox(
+            height: 200,
+            width: double.infinity,
+            child: _image,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Row(
+              children: [
+                Text(
+                  _name,
+                  style: Helper.getTheme(context)
+                      .headline4!
+                      .copyWith(color: AppColor.primary),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              children: [
+                Image.asset(
+                  Helper.getAssetName("star_filled.png", "virtual"),
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  "4.9",
+                  style: TextStyle(color: AppColor.orange),
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Text("(124 ratings) cafe"),
+                SizedBox(
+                  width: 5,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 5),
+                  child: Text(
+                    ".",
+                    style: TextStyle(
+                      color: AppColor.orange,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text("Western Food"),
+              ],
             ),
           ),
         ],
