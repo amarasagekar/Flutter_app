982a7bdc9fb5cb1c812b7c61477004e4aa34926e
Amar Asagekar <amarasagekar@gmail.com>
2023-10-27 11:39:09 +0530

    Home page UI and api

--- online_shop/lib/views/ui/homepage.dart
+++ online_shop/lib/views/ui/homepage.dart
@@ -1,6 +1,13 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:online_shop/models/sneaker_model.dart';
+import 'package:online_shop/services/helper.dart';
 import 'package:online_shop/views/shared/appstyle.dart';
+import 'package:online_shop/views/shared/product_card.dart';
+
+import '../shared/home_widget.dart';
+import '../shared/new_shoes.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,9 +20,34 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   late final TabController _tabController =
       TabController(length: 3, vsync: this);
 
+  late Future<List<Sneakers>> _male;
+  late Future<List<Sneakers>> _female;
+  late Future<List<Sneakers>> _kids;
+
+  void getMale() {
+    _male = Helper().getMaleSneakers();
+  }
+
+  void getFemale() {
+    _female = Helper().getFemaleSneakers();
+  }
+
+  void getKids() {
+    _kids = Helper().getKidsSneakers();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getMale();
+    getFemale();
+    getKids();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFFE2E2E2),
       body: SizedBox(
         height: MediaQuery.of(context).size.height,
         child: Stack(
@@ -46,6 +78,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
                           42, Colors.white, FontWeight.bold, 1.2),
                     ),
                     TabBar(
+                      padding: EdgeInsets.zero,
                       indicatorSize: TabBarIndicatorSize.label,
                       indicatorColor: Colors.transparent,
                       controller: _tabController,
@@ -71,62 +104,17 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
             ),
             Padding(
               padding: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.height * 0.265),
-              child: TabBarView(
-                controller: _tabController,
-                children: [
-                  Column(
-                    children: [
-                      Container(
-                        height: MediaQuery.of(context).size.height * 0.405,
-                        color: Colors.amber,
-                      ),
-                      Column(
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                "Latest Shoe",
-                                style:
-                                    appstyle(24, Colors.black, FontWeight.bold),
-                              ),
-                              Row(
-                                children: [
-                                  Text(
-                                    "Show All",
-                                    style: appstyle(
-                                        22, Colors.black, FontWeight.w500),
-                                  ),
-                                   Icon(AntDesign.caretright, size: 20,),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.15,
-                      ),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Container(
-                        height: MediaQuery.of(context).size.height * 0.405,
-                        color: Colors.green,
-                      ),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Container(
-                        height: MediaQuery.of(context).size.height * 0.405,
-                        color: Colors.amber,
-                      ),
-                    ],
-                  ),
-                ],
+                  top: MediaQuery.of(context).size.height * 0.25),
+              child: Container(
+                padding: const EdgeInsets.only(left: 12),
+                child: TabBarView(
+                  controller: _tabController,
+                  children: [
+                    HomeWidget(male: _male),
+                    HomeWidget(male: _male),
+                    HomeWidget(male: _male),
+                  ],
+                ),
               ),
             ),
           ],
--- online_shop/lib/views/ui/mainscreen.dart
+++ online_shop/lib/views/ui/mainscreen.dart
@@ -34,4 +34,3 @@ class MainScreen extends StatelessWidget {
     );
   }
 }
-
