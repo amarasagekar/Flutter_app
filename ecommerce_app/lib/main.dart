a074c91ed45d275a97de0465dd6e039a759414e3
Amar Asagekar <amarasagekar@gmail.com>
2023-11-23 17:11:47 +0530

    admin page UI

--- ecommerce_app/lib/main.dart
+++ ecommerce_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app/common/widgets/bottom_bar.dart';
+import 'package:ecommerce_app/features/admin/screens/admin_screen.dart';
 import 'package:path/path.dart' as Path;
 
 import 'package:ecommerce_app/constants/global_variables.dart';
@@ -39,24 +40,23 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
-      title: 'Amazon Clone',
-      theme: ThemeData(
-        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
-        colorScheme: const ColorScheme.light(
-          primary: GlobalVariables.secondaryColor,
+        debugShowCheckedModeBanner: false,
+        title: 'Amazon Clone',
+        theme: ThemeData(
+          scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+          colorScheme: const ColorScheme.light(
+            primary: GlobalVariables.secondaryColor,
+          ),
+          appBarTheme: const AppBarTheme(
+            elevation: 0,
+            iconTheme: IconThemeData(color: Colors.black),
+          ),
         ),
-        appBarTheme: const AppBarTheme(
-          elevation: 0,
-          iconTheme: IconThemeData(color: Colors.black),
-        ),
-      ),
-      
-      onGenerateRoute: (settings) => generateRoute(settings),
-      
-      home: Provider.of<UserProvider>(context).user.token.isNotEmpty
-          ? const BottomBar()
-          : const AuthScreen(),
-    );
+        onGenerateRoute: (settings) => generateRoute(settings),
+        home: Provider.of<UserProvider>(context).user.token.isNotEmpty
+            ? Provider.of<UserProvider>(context).user.type == 'user'
+                ? const BottomBar()
+                : const AdminScreen()
+            : const AuthScreen());
   }
 }
--- ecommerce_app/lib/router.dart
+++ ecommerce_app/lib/router.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app/common/widgets/bottom_bar.dart';
+import 'package:ecommerce_app/features/admin/screens/add_product_screen.dart';
 import 'package:ecommerce_app/features/auth/screens/auth_screen.dart';
 import 'package:ecommerce_app/features/home/screens/home_screen.dart';
 import 'package:flutter/material.dart';
@@ -10,15 +11,20 @@ Route<dynamic> generateRoute(RouteSettings routeSettings) {
         settings: routeSettings,
         builder: (_) => const AuthScreen(),
       );
-      case HomeScreen.routeName:
+    case HomeScreen.routeName:
       return MaterialPageRoute(
         settings: routeSettings,
         builder: (_) => const HomeScreen(),
       );
-      case BottomBar.routeName:
+    case BottomBar.routeName:
       return MaterialPageRoute(
         settings: routeSettings,
         builder: (_) => const BottomBar(),
+      );
+    case AddProductScreen.routeName:
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => const AddProductScreen(),
       );
     default:
       return MaterialPageRoute(
