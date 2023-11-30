bb88e5da3ca5a5c600e09466a4ae58ee33204c81
Amar Asagekar <amarasagekar@gmail.com>
2023-11-30 22:00:48 +0530

    completed admin section - product delete, product list API

--- ecommerce_app/lib/features/admin/screens/posts_screen.dart
+++ ecommerce_app/lib/features/admin/screens/posts_screen.dart
@@ -1,3 +1,5 @@
+import 'package:ecommerce_app/common/widgets/loader.dart';
+import 'package:ecommerce_app/features/account/widgets/single_product.dart';
 import 'package:ecommerce_app/features/admin/screens/add_product_screen.dart';
 import 'package:ecommerce_app/features/admin/services/admin_services.dart';
 import 'package:ecommerce_app/models/product.dart';
@@ -21,7 +23,19 @@ class _PostsScreenState extends State<PostsScreen> {
   }
 
   fetchAllProducts() async {
-    await adminServices.fetchAllProducts(context);
+    products = await adminServices.fetchAllProducts(context);
+    setState(() {});
+  }
+
+  void deleteProduct(Product product, int index) {
+    adminServices.deleteProduct(
+      context: context,
+      product: product,
+      onSuccess: () {
+        products!.removeAt(index);
+        setState(() {});
+      },
+    );
   }
 
   void navigateToAddProduct() {
@@ -30,16 +44,50 @@ class _PostsScreenState extends State<PostsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return products == null ?  : Scaffold(
-      body: const Center(
-        child: Text('Products'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: navigateToAddProduct,
-        tooltip: 'Add a Product',
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-    );
+    return products == null
+        ? const Loader()
+        : Scaffold(
+            body: GridView.builder(
+              itemCount: products!.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2),
+              itemBuilder: (context, index) {
+                final productData = products![index];
+                return Column(
+                  children: [
+                    SizedBox(
+                      height: 140,
+                      child: SingleProduct(
+                        image: productData.images[0],
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Expanded(
+                          child: Text(
+                            productData.name,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 2,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () => deleteProduct(productData, index),
+                          icon: const Icon(Icons.delete_outline),
+                        ),
+                      ],
+                    ),
+                  ],
+                );
+              },
+            ),
+            floatingActionButton: FloatingActionButton(
+              child: const Icon(Icons.add),
+              onPressed: navigateToAddProduct,
+              tooltip: 'Add a Product',
+            ),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerFloat,
+          );
   }
 }
