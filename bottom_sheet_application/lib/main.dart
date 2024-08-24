62127bc0c8cfa0f5b5cb1975a2257912520c78b9
“amarasagekar” <amarasagekar@gmail.com>
2024-08-24 22:27:33 +0530

    Added dummy UI and complted bottom sheet

--- bottom_sheet_application/lib/main.dart
+++ bottom_sheet_application/lib/main.dart
@@ -14,11 +14,72 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: DraggableSheet(
-          child: SizedBox(
-            height: 100,
+          child: Column(
+            children: [
+              BottomSheetDummyUI(),
+              BottomSheetDummyUI(),
+              BottomSheetDummyUI(),
+              BottomSheetDummyUI(),
+              BottomSheetDummyUI(),
+              BottomSheetDummyUI(),
+              BottomSheetDummyUI(),
+            ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class BottomSheetDummyUI extends StatelessWidget {
+  const BottomSheetDummyUI({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Container(
+          padding: EdgeInsets.only(left: 30, right: 30),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(15.0),
+                    child: Container(
+                      color: Colors.black12,
+                      height: 100,
+                      width: 100,
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(15.0),
+                        child: Container(
+                          color: Colors.black12,
+                          height: 20,
+                          width: 240,
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(15.0),
+                        child: Container(
+                          color: Colors.black12,
+                          height: 20,
+                          width: 180,
+                        ),
+                      ),
+                      SizedBox(height: 50),
+                    ],
+                  )
+                ],
+              ),
+              SizedBox(height: 10),
+            ],
+          )),
     );
   }
 }
